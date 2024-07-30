876a41f1ab2e6006a47c387a940e63e4314379a6
gabriel-vitebo <vitebo@outlook.com>
2024-07-30 15:05:16 -0300

    create theme styles

---
+++ src/styles/theme.ts
@@ -0,0 +1,20 @@
+export default {
+  COLORS: {
+    primary: '#9DA44E',
+    secondary: '#2E2D2D',
+    black: '#000000',
+    white: '#FFFFFF',
+    blackBG: '#1E1D1D',
+    whiteBG: '#AEACAC',
+    greenBG: '#60B852',
+    greenBgHover: '#3fa62d',
+    redBG: '#9D3D3D',
+    redBgHover: '#e22626',
+  },
+
+  FONTS: {
+    primary: "'Kameron', sans-serif",
+    default: "'Inter', sans-serif",
+    highLight: "'Koulen', sans-serif",
+  },
+}
